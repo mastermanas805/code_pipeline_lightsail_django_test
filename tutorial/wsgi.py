51703fa1dbd048b74ee55478f8b78266d6699d1b
Your Name <manas10@live.com>
2021-09-28 12:08:34 +0530

    init

---
+++ tutorial/wsgi.py
@@ -0,0 +1,8 @@
+mport os
+import sys
+sys.path.append('/opt/bitnami/projects/tutorial')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/tutorial/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorial.settings")
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
