5c615560be924f00a5efd26de7b3a67b8bc66099
Your Name <manas10@live.com>
2021-10-08 11:40:08 +0530

    script bugfixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt install python3-pip python3-dev -y
+sudo apt install python3-pip python3-dev python3.8-venv -y
 sudo -H pip3 install virtualenv django
-set_add = "/home/ubuntu/code_pipeline_lightsail_django_test/"
-sudo python3 -m venv $set_add/venv &&
+set_add="/home/ubuntu/code_pipeline_lightsail_django_test/"
+sudo python3 -m venv $set_add/venv
 sudo source $set_add/venv/bin/activate
-&& sudo pip3 install django gunicorn
+sudo pip3 install django gunicorn
 
 
 
