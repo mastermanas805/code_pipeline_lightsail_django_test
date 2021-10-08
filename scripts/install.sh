6bf93f501c731a28421c76b0047507437aed1e52
Your Name <manas10@live.com>
2021-10-08 11:03:49 +0530

    Nginx port 80 hosting

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt install python3-pip python3-dev -y
 sudo -H pip3 install virtualenv django
+sudo cd /home/ubuntu/code_pipeline_lightsail_django_test && sudo python3 -m venv venv && sudo pip3 install django gunicorn
+
+
+
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
-log=log.txt
+sudo echo "
+[Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
 
-python3 manage.py runserver 0.0.0.0:8000 >> $log
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/code_pipeline_lightsail_django_test
+ExecStart=/home/sammy/code_pipeline_lightsail_django_test/venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          hello.wsgi:application
+
+[Install]
+WantedBy=multi-user.target
+" 
+> /etc/systemd/system/gunicorn.service
+
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
+
+sudo echo 
+"
+server {
+    listen 80;
+    server_name 35.154.218.78;
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static/ {
+        root /home/ubuntu/code_pipeline_lightsail_django_test;
+    }
+
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn.sock;
+    }
+}
+" 
+> /etc/nginx/sites-available/hello
+
+sudo ln -s /etc/nginx/sites-available/hello /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full'
