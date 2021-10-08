cbb5c1d0a9a1881004785e39315f0c30986023ae
Your Name <manas10@live.com>
2021-10-08 21:48:00 +0530

    install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 sudo apt install python3-pip python3-dev python3.8-venv -y
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv django virtualenv
-set_add="/home/ubuntu/code_pipeline_lightsail_django_test/"
+set_add="/home/ubuntu/code_pipeline_lightsail_django_test"
 sudo virtualenv $set_add/venv
 activate(){
     $set_add/venv/bin/activate
