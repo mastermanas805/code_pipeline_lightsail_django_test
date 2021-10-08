78d3c0f8a9da80ae5c490199486b2a34d260bbbd
Your Name <manas10@live.com>
2021-10-08 21:58:05 +0530

    gunicorn.service venv

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,6 @@ sudo apt install python3-pip python3-dev python3.8-venv -y
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv django virtualenv
 set_add="/home/ubuntu/code_pipeline_lightsail_django_test"
-sudo virtualenv $set_add/venv
-activate(){
-    $set_add/venv/bin/activate
-    pip3 install django gunicorn
-}
-activate
-
-
+pip3 install django gunicorn
 
 
