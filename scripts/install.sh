d2932f0a2c8831c65a0bfccc83f0d34d5e58fcfb
Your Name <manas10@live.com>
2021-10-08 15:40:17 +0530

    changed virtual env

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt install python3-pip python3-dev python3.8-venv -y
-sudo -H pip3 install virtualenv django
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install virtualenv django virtualenv
 set_add="/home/ubuntu/code_pipeline_lightsail_django_test/"
-sudo python3 -m venv $set_add/venv
+sudo virtualenv $set_add/venv
 sudo source $set_add/venv/bin/activate
 sudo pip3 install django gunicorn
 
